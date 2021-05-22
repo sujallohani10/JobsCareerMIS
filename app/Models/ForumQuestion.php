cb2031fedb55cf06d5a7ddd5b327f63e711c65ce
sujallohani10 <sujal.lohani7@gmail.com>
2021-05-22 14:29:21 +1000

    forum database migration created..

---
+++ app/Models/ForumQuestion.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ForumQuestion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'question_title',
+        'question_description',
+    ];
+}
---
+++ database/migrations/2021_05_22_041743_create_forum_questions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateForumQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('forum_questions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->string('question_title');
+            $table->text('question_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('forum_questions');
+    }
+}
